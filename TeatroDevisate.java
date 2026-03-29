ebbccecfc233a7f08564688cf349036023bb7f04
kut007 <antonio.rita.fer@gmail.com>
2026-03-29 20:47:38 -0300

    Teatro finalizado.

--- TeatroDevisate.java
+++ TeatroDevisate.java
@@ -20,6 +20,7 @@ class Teatro {
     int totalSalas = 0;
 
     String[] titulos = new String[20];
+    String[] enunciados = new String[20];
     Scanner sc = new Scanner(System.in);
 
     public Teatro() {
@@ -35,9 +36,47 @@ class Teatro {
 
         totalSalas = 2;
         
-        for (int i = 0; i < 20; i++) {
-            titulos[i] = "Exercicio " + (i + 1);
-        }
+        titulos[0]  = "Exercicio 01 - Par ou Impar";
+        titulos[1]  = "Exercicio 02 - Maior de Dois Numeros";
+        titulos[2]  = "Exercicio 03 - Aprovacao Escolar";
+        titulos[3]  = "Exercicio 04 - Classificacao de Temperatura";
+        titulos[4]  = "Exercicio 05 - Calculadora Simples com Switch";
+        titulos[5]  = "Exercicio 06 - Ano Bissexto";
+        titulos[6]  = "Exercicio 07 - Triangulo Valido";
+        titulos[7]  = "Exercicio 08 - IMC";
+        titulos[8]  = "Exercicio 09 - Dia da Semana com Switch";
+        titulos[9]  = "Exercicio 10 - Positivo, Negativo ou Zero";
+        titulos[10] = "Exercicio 11 - Ingresso de Cinema";
+        titulos[11] = "Exercicio 12 - Nota por Conceito";
+        titulos[12] = "Exercicio 13 - Estacao do Ano com Switch";
+        titulos[13] = "Exercicio 14 - Maior de Tres Numeros";
+        titulos[14] = "Exercicio 15 - Verificador de Login";
+        titulos[15] = "Exercicio 16 - Multiplo de 3 e 5";
+        titulos[16] = "Exercicio 17 - Desconto em Compra";
+        titulos[17] = "Exercicio 18 - Numero Romano com Switch";
+        titulos[18] = "Exercicio 19 - Velocidade e Multa";
+        titulos[19] = "Exercicio 20 - Pedra, Papel e Tesoura";
+
+        enunciados[0]  = "Leia um inteiro e informe se e par ou impar. Use operador ternario.";
+        enunciados[1]  = "Leia dois inteiros e exiba o maior. Use operador ternario.";
+        enunciados[2]  = "Leia a media (0-10): Aprovado >= 7, Recuperacao >= 5, Reprovado < 5.";
+        enunciados[3]  = "Leia a temperatura: Frio < 15, Agradavel 15-25, Quente > 25. Use ternario aninhado.";
+        enunciados[4]  = "Leia dois numeros e operador (+,-,*,/). Use switch. Trate divisao por zero.";
+        enunciados[5]  = "Leia um ano e informe se e bissexto (div. por 4, exceto centenarios salvo div. por 400).";
+        enunciados[6]  = "Leia tres lados: verifique triangulo valido e classifique (equilatero/isosceles/escaleno).";
+        enunciados[7]  = "Leia peso e altura, calcule IMC (peso/altura^2): Abaixo do peso, Normal, Sobrepeso, Obesidade.";
+        enunciados[8]  = "Leia numero de 1-7 e exiba o dia da semana. Use switch. Fora do intervalo: 'Dia invalido'.";
+        enunciados[9]  = "Leia um numero e informe se e positivo, negativo ou zero. Use ternario aninhado.";
+        enunciados[10] = "Cinema: menor 12 anos R$10, entre 12-60 R$20, acima 60 R$10. Leia idade e exiba valor.";
+        enunciados[11] = "Leia nota (0-10) e exiba conceito: A (9-10), B (7-8), C (5-6), D (0-4).";
+        enunciados[12] = "Leia mes (1-12) e exiba a estacao do ano no hemisferio sul. Use switch.";
+        enunciados[13] = "Leia tres inteiros e exiba o maior. Trate empate.";
+        enunciados[14] = "Usuario e senha fixos no codigo. Leia e exiba 'Acesso permitido' ou 'Acesso negado'. Use ternario.";
+        enunciados[15] = "Leia inteiro: multiplo de 3 e 5, so de 3, so de 5, ou nenhum. Use ternario onde possivel.";
+        enunciados[16] = "Compra: acima R$500 = 20% desc, R$200-500 = 10% desc, abaixo R$200 = sem desc. Exiba valor final.";
+        enunciados[17] = "Leia numero de 1-10 e exiba em romano. Use switch.";
+        enunciados[18] = "Leia velocidade e limite: Sem multa, Leve (ate 20%), Grave (20-50%), Gravissima (>50%).";
+        enunciados[19] = "Dois jogadores escolhem pedra/papel/tesoura. Determine vencedor ou empate. Use ternario no resultado.";
 
         menuPrincipal();
     }
@@ -226,11 +265,38 @@ class Teatro {
 
             if (acao.equals("P") && pag < 2) pag++;
             else if (acao.equals("A") && pag > 0) pag--;
-            else if (!acao.equals("V")) executarExercicio(); 
+            else if (!acao.equals("V")) executarExercicio(acao);
             
         } while (!acao.equals("V"));
     }
 
-    void executarExercicio() {
+    void executarExercicio(String acao) {
+        int num = Integer.parseInt(acao);
+
+        System.out.println(titulos[num - 1] );
+        System.out.println(enunciados[num - 1]);
+
+        switch (num) {
+            case 1:  Exercicio_01.main(new String[]{}); break;
+            case 2:  Exercicio_02.main(new String[]{}); break;
+            case 3:  Exercicio_03.main(new String[]{}); break;
+            case 4:  Exercicio_04.main(new String[]{}); break;
+            case 5:  Exercicio_05.main(new String[]{}); break;
+            case 6:  Exercicio_06.main(new String[]{}); break;
+            case 7:  Exercicio_07.main(new String[]{}); break;
+            case 8:  Exercicio_08.main(new String[]{}); break;
+            case 9:  Exercicio_09.main(new String[]{}); break;
+            case 10: Exercicio_10.main(new String[]{}); break;
+            case 11: Exercicio_11.main(new String[]{}); break;
+            case 12: Exercicio_12.main(new String[]{}); break;
+            case 13: Exercicio_13.main(new String[]{}); break;
+            case 14: Exercicio_14.main(new String[]{}); break;
+            case 15: Exercicio_15.main(new String[]{}); break;
+            case 16: Exercicio_16.main(new String[]{}); break;
+            case 17: Exercicio_17.main(new String[]{}); break;
+            case 18: Exercicio_18.main(new String[]{}); break;
+            case 19: Exercicio_19.main(new String[]{}); break;
+            case 20: Exercicio_20.main(new String[]{}); break;
+        }
     }
 }
